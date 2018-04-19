15734beb834f5b865aa0782c3420dd4f59a9d992
Kucherenko Dmytrii <kucherenko.dmt@gmail.com>
2018-04-19 21:23:40 +0300

    add dependenci

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,9 +1,26 @@
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class SimpleTest {
     @Test
     public void simpleValidation(){
-        Assert.assertEquals(7, 4+4);
+        Assert.assertEquals(7, 4+3);
+    }
+    @Test
+    public void checkThatFierfox(){
+        String valueFromProfile;
+        Properties properties = new Properties();
+        InputStream stream = Main.class.getResourceAsStream("/config.properties");
+        try {
+            properties.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        valueFromProfile = properties.getProperty("driverType");
+        Assert.assertEquals("fierfox", valueFromProfile);
     }
 }
