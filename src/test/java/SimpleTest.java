5edf22e9bcd2a7f78f89f0bc6fc55f792f9444a8
Kucherenko Dmytrii <kucherenko.dmt@gmail.com>
2018-04-19 20:40:37 +0300

    first commit

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class SimpleTest {
+    @Test
+    public void simpleValidation(){
+        Assert.assertEquals(7, 4+4);
+    }
 }
